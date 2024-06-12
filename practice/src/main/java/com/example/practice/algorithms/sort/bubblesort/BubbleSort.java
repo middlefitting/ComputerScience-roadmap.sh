d8e377313a0eb06800c8129eb30e33ca285fda44
middlefitting <wjdtmdcjf2@gmail.com>
2024-06-12 14:20:27 +0900

    feat(algorithms): bubblesort

--- practice/src/main/java/com/example/practice/algorithms/sort/bubblesort/BubbleSort.java
+++ practice/src/main/java/com/example/practice/algorithms/sort/bubblesort/BubbleSort.java
@@ -12,11 +12,24 @@ package com.example.practice.algorithms.sort.bubblesort;
  * @since : 2024/06/12
  */
 public class BubbleSort {
-	public static int[] sort(int[] arr) {
-		for (int i = arr.length - 1; i > 0; i--) {
-			for (int j = i; j < arr.length; j++) {
+	public static int[] sort(int[] arr, boolean reverse) {
+		int temp;
+		boolean isSwapped;
 
+		for (int i = arr.length; i > 0; i--) {
+			isSwapped = false;
+			for (int j = 0; j < i - 1; j++) {
+				if (arr[j] > arr[j + 1] == !reverse) {
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					isSwapped = true;
+				}
+			}
+			if (!isSwapped) {
+				break;
 			}
 		}
+		return arr;
 	}
 }
---
+++ practice/src/test/java/com/example/practice/algorithms/sort/bubblesort/BubbleSortTest.java
@@ -0,0 +1,31 @@
+package com.example.practice.algorithms.sort.bubblesort;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BubbleSortTest {
+	@Test
+	@DisplayName("Order must be Success")
+	void test() {
+		//Arrange
+		int[] arr = {5, 4, 3, 2, 1};
+		int[] arr2 = {3, 4, 3, -2, 1};
+
+		//Act sort
+		int[] result = BubbleSort.sort(arr, false);
+		int[] result2 = BubbleSort.sort(arr2, false);
+
+		//Assert sort
+		Assertions.assertThat(result).isSorted();
+		Assertions.assertThat(result2).isSorted();
+
+		//Act reverse sort
+		int[] result3 = BubbleSort.sort(arr, true);
+		int[] result4 = BubbleSort.sort(arr2, true);
+
+		//Assert reverse sort
+		Assertions.assertThat(result3).isSortedAccordingTo((a, b) -> b - a);
+		Assertions.assertThat(result4).isSortedAccordingTo((a, b) -> b - a);
+	}
+}
