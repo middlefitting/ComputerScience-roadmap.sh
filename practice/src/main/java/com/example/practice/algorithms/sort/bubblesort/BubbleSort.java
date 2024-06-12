7948d23b52b2124118a22ee9cbd45828bade5c8c
middlefitting <wjdtmdcjf2@gmail.com>
2024-06-12 13:52:35 +0900

    chore: environments

---
+++ practice/src/main/java/com/example/practice/algorithms/sort/bubblesort/BubbleSort.java
@@ -0,0 +1,22 @@
+package com.example.practice.algorithms.sort.bubblesort;
+
+/**
+ * BubbleSort.
+ *
+ * <p>
+ *
+ * </p>
+ *
+ * @author : middlefitting
+ * @see :
+ * @since : 2024/06/12
+ */
+public class BubbleSort {
+	public static int[] sort(int[] arr) {
+		for (int i = arr.length - 1; i > 0; i--) {
+			for (int j = i; j < arr.length; j++) {
+
+			}
+		}
+	}
+}
