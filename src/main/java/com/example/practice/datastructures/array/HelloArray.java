f54a4350f4e64cf2735bad2b8963ad597db09772
middlefitting <wjdtmdcjf2@gmail.com>
2024-06-21 16:45:40 +0900

    feat(datastructures): 정적 배열과 동적 배열 선언하기

---
+++ src/main/java/com/example/practice/datastructures/array/HelloArray.java
@@ -0,0 +1,14 @@
+package com.example.practice.datastructures.array;
+
+import java.util.ArrayList;
+
+public class HelloArray {
+	public static void main(String[] args) {
+		// 정적 배열
+		int[] staticArr = new int[5];
+		int[] staticArr2 = {1, 2, 3, 4, 5};
+
+		// 동적 배열
+		ArrayList<Integer> dynamicArray = new ArrayList<>(10); // 초기 용량 할당
+	}
+}
