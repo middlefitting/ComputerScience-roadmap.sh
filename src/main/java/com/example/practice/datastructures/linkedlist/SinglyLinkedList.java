d72fefd933e82966db29ffde76a98c3c2a1293e3
middlefitting <wjdtmdcjf2@gmail.com>
2024-06-22 19:04:24 +0900

    docs(datastructures): LinkedList README

---
+++ src/main/java/com/example/practice/datastructures/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,13 @@
+package com.example.practice.datastructures.linkedlist;
+/**
+ * SinglyLinkedList.
+ *
+ * <p>
+ *   
+ * </p>
+ * @see             : 
+ * @author          : middlefitting
+ * @since           : 2024/06/22
+ */
+public class SinglyLinkedList {
+}
